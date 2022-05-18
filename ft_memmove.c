9d34b3f5693cc5cdf6e39c407ed8ad448415cf19
pgaric <pgaric@student.42wolfsburg.de>
2022-05-18 12:20:38 +0200

    2

--- ft_memmove.c
+++ ft_memmove.c
@@ -18,16 +18,16 @@ void	*ft_memmove(void *dest, const void *src, size_t n)
 	unsigned char	*d;
 	size_t			i;
 
+	if (dest == NULL || src == NULL)
+		return (NULL);
 	s = (unsigned char *)src;
 	d = (unsigned char *)dest;
 	i = 0;
 	if ((size_t)d - (size_t)s < n)
 	{
-		i = n - 1;
-		while (i < n)
+		while (n--)
 		{
-			d[i] = s[i];
-			i--;
+			d[n] = s[n];
 		}
 	}
 	else
