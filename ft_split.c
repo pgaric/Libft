dffd72d5c8ed4e6464fb9e66e6cd8cce4f6bbcfc
pgaric <pgaric@student.42wolfsburg.de>
2022-05-19 12:10:13 +0200

    change

--- ft_split.c
+++ ft_split.c
@@ -37,9 +37,9 @@ char	**ft_split(char const *s, char c)
 	int		j;
 	int		start;
 
-	if (!s || !c)
-		return (NULL);
 	arr = malloc(sizeof(char *) * (wordcount(s, c) + 1));
+	if (!arr || !s)
+		return (NULL);
 	j = 0;
 	i = 0;
 	while (s[i])
